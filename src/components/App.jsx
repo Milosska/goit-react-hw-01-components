84d58dd3fa957cd64f9b55dac28f4938fcd7089b
Milosska <110258611+Milosska@users.noreply.github.com>
2023-02-15 23:36:26 +0200

    first two sections

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,13 @@
+import user from '../data/user';
+import data from '../data/data';
+import { Profile } from './Profile/Profile';
+import { Statistics } from './Statistics/Statistics';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile user={user} />
+      <Statistics stats={data} />
     </div>
   );
 };
